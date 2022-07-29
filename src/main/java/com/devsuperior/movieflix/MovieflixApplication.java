b920ba59f7f6bcbdcc2f74d64b9a27608f49712d
Luis Ribeiro <luis.ribeiro@unicred.com.br>
2022-07-29 20:34:04 -0300

    Entity Genre and Movie. Seed Genre.

--- src/main/java/com/devsuperior/movieflix/MovieflixApplication.java
+++ src/main/java/com/devsuperior/movieflix/MovieflixApplication.java
@@ -2,8 +2,10 @@ package com.devsuperior.movieflix;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
 public class MovieflixApplication {
 
 	public static void main(String[] args) {
