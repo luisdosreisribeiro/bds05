5e30e0baa9e84e7b9894d24b0c2f7666306cb180
Luis Ribeiro <luis.ribeiro@unicred.com.br>
2022-07-30 10:11:04 -0300

    Role.

--- src/main/java/com/devsuperior/movieflix/entities/User.java
+++ src/main/java/com/devsuperior/movieflix/entities/User.java
@@ -1,13 +1,18 @@
 package com.devsuperior.movieflix.entities;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
@@ -24,6 +29,12 @@ public class User {
 	
 	@OneToMany(mappedBy = "user")	
 	private List<Review> reviews = new ArrayList<>();
+	
+	@ManyToMany(fetch = FetchType.EAGER)
+	@JoinTable(name = "tb_user_role",
+	joinColumns = @JoinColumn(name = "user_id"),
+	inverseJoinColumns = @JoinColumn(name = "role_id"))
+	private Set<Role>roles = new HashSet<>();
 	
 	public User() {
 		
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,13 @@
+INSERT INTO tb_user(name, email, password)VALUES('bob','bob@gmail.com', '$2a$10$eACCYoNOHEqXve8aIWT8Nu3PkMXWBaOxJ9aORUYzfMQCbVBIhZ8tG');
+INSERT INTO tb_user(name, email, password)VALUES('ana','ana@gmail.com', '$2a$10$eACCYoNOHEqXve8aIWT8Nu3PkMXWBaOxJ9aORUYzfMQCbVBIhZ8tG');
+
+INSERT INTO tb_role (authority) VALUES ('ROLE_VISITOR');
+INSERT INTO tb_role (authority) VALUES ('ROLE_MEMBER');
+
+INSERT INTO tb_user_role (user_id, role_id) VALUES (1, 1);
+INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 2);
+
+
 INSERT INTO tb_genre(name)VALUES('Ação');
 INSERT INTO tb_genre(name)VALUES('Comédia');
 INSERT INTO tb_genre(name)VALUES('Romance');
